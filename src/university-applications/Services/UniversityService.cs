602ba69b7142f1ac9223676b194e2f3ba23837e8
MaxRudim <max.hermsdorff@gmail.com>
2022-10-18 17:19:32 -0300

    Implementação da integração com a API Universities

--- src/university-applications/Services/UniversityService.cs
+++ src/university-applications/Services/UniversityService.cs
@@ -2,9 +2,42 @@ namespace university_applications.Services
 {
   public class UniversityService : IUniversityService
   {
-    public UniversityService()
+    private readonly HttpClient _client;
+    public UniversityService(HttpClient httpClient)
     {
-      throw new System.NotImplementedException();
+      _client = httpClient;
+    }
+  
+    public async Task<object> FindUniversity(string name, string country)
+    {
+      var response = await _client.GetAsync($"http://universities.hipolabs.com/search?name={name}&country={country}");
+
+      if (!response.IsSuccessStatusCode)
+      {
+        return null!;
+      }
+
+      var result = await response.Content.ReadFromJsonAsync<object>();
+
+      if (result!.ToString() == "[]") return null!;
+
+      else return result;
+    }
+
+    public async Task<object> FindUniversity(string country)
+    {
+      var response = await _client.GetAsync($"http://universities.hipolabs.com/search?country={country}");
+
+      if (!response.IsSuccessStatusCode)
+      {
+        return null!;
+      }
+
+      var result = await response.Content.ReadFromJsonAsync<object>();
+
+      if (result!.ToString() == "[]") return null!;
+      
+      else return result;
     }
   }
 }
